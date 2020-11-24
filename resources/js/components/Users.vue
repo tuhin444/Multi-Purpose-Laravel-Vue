b46045643eec435ce127cde5c4bf27658379ad40
tuhin444 <tuhincse444@gmail.com>
2020-11-25 04:51:41 +0600

    intitial commit

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,16 +1,46 @@
 
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Users Component</div>
+       <div class="row mt-5">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">User Mangment</h3>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
+                <div class="card-tools">
+                      <button type="submit" class="btn btn-success"><i class="fas fa-user-plus"></i></button>
                 </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Modify</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>175</td>
+                      <td>Mike Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-danger">Denied</span></td>
+                      <td>
+                         <a href=""><i class="fas fa-edit"></i></a>
+                         <a href=""><i class="fas fa-trash-alt"></i></a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
             </div>
+            <!-- /.card -->
+          </div>
         </div>
     </div>
 </template>
