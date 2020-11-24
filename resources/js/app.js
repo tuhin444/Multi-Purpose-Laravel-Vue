724cf3e4dae255a542d2e545ce5dc7a515d2cb83
tuhin444 <tuhincse444@gmail.com>
2020-11-24 23:41:49 +0600

    Vou router setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,18 +6,26 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 
-let routes = [
-  { path: '/dashboard', component: require('./components/Dashboard.vue')},
-  { path: '/profile', component: require('./components/Profile.vue')}
+
+let routes = [{
+    path: '/dashboard',
+    component: require('./components/Dashboard.vue')
+  },
+  {
+    path: '/profile',
+    component: require('./components/Profile.vue')
+  }
 ]
 
 
 const router = new VueRouter({
+	mode: 'history',
   routes // short for `routes: routes`
 })
 
 //Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
+	el:'#app',
   router
 });
--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -1,13 +1,28 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <h1>Profile</h1>
+<div class="container">
+  <div class="row justify-content-center">
+     <div class="col-md-8">
+        <div class="card card-default">
+           <div class="card-header">
+              <h1>Profile</h1>
+           </div>
+           <div class="card-body">
+             <h1>Card body profile</h1>
+           </div>
         </div>
-    </div>
+     </div>
+  </div>
+</div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  mounted() {
+            console.log('Component mounted.')
+        }
+}
 </script>
+
+<style>
+
+</style>
